69f10e9a1d2dd3bc532b968d61230038f748db21
CharlesdeGore <rrouault11@gmail.com>
2018-06-11 10:52:58 +0200

    Add Symfony Security Component

--- SnowTricks/src/Domain/DataFixtures/Fixtures.php
+++ SnowTricks/src/Domain/DataFixtures/Fixtures.php
@@ -10,27 +10,43 @@
 namespace App\Domain\DataFixtures;
 
 use App\Domain\Entity\Trick;
+use App\Domain\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+//use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class Fixtures extends Fixture
 {
 
+    private $encoder;
+
+    /*public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }*/
+
     public function load(ObjectManager $manager)
     {
 
-        // create 5 tricks
         for ($i = 0; $i < 5; $i++) {
+            //create 5 users
+            $user = new User;
+            $user->setUserPseudo('User'. $i);
+            $user->setUserMail('User'. $i .'@mail.com');
+            $user->setUserPass();
+            // create 5 tricks
             $trick = new Trick();
-            $trick->setTrickName('trick ' . $i);
-            $trick->setTrickDescription('An awesome trick');
+            $trick->setTrickName('Trick ' . $i);
+            $trick->setTrickDescription(array_rand(array('An awesome trick to shine on the slopes!', 'A great trick for confirmed snowboarder.', 'The 1080° classical!', 'A trick in which the rider\'s rear hand grabs the heel side of the board front for the front bindings.', 'The front hand grabs the toe edge in between the feet and the front knee is pulled to the board.'), 1));
             $trick->setTrickCreation(new \DateTime(sprintf('-%d days', rand(1, 100))));
 
-            $trick->persist($trick);
+            $manager->persist($trick);
         }
 
         $manager->flush();
     }
 
+
 }
+
